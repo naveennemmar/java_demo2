7bf42172172565a23e59b41896294750305e7848
naveennemmar <159225223+naveennemmar@users.noreply.github.com>
2024-02-13 20:09:15 -0800

    Add files via upload

---
+++ classdistance.java
@@ -0,0 +1,15 @@
+class distance
+{
+  int meters;
+  int cms;
+public void setdata(int m,int cm)
+ {
+   meters=m;
+   cms=cm;
+  }
+public void display()
+  {
+   System.out.print1n("meters="+meters);
+   System.out.print1n("cms="+cms);
+   }
+}
